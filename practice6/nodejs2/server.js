2096214488e71d0d6eb3390fbc68adf41c2c981b
teru12012000 <sion0012@outlook.jp>
2022-03-08 20:23:14 +0900

    チャット機能演習

---
+++ practice6/nodejs2/server.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const http=require("http");
+const { Socket } = require("socket.io");
+const server=http.createServer(app);
+const io=require("socket.io")(server);
+const PORT=3000;
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=>{
+  console.log("ユーザーが接続しました.");
+
+  socket.on("chat message",(msg)=>{
+    io.emit("chat message",msg);
+  });
+})
+server.listen(PORT,()=>{
+  console.log("GOOD!");
+});
