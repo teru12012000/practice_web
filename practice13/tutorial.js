0c139367aaeb03a039c79e59e2bb9d62b34fba54
teru12012000 <sion0012@outlook.jp>
2022-04-15 15:59:45 +0900

    ts入門

---
+++ practice13/tutorial.js
@@ -0,0 +1,8 @@
+//const a:Number=3;
+//const b:Number=a;//tsでは変数の後ろに型を入れる
+//console.log(b);
+//let array1:Array<number>=[1,2,3,4];
+var array1 = [1, 2, 3, 4];
+var array2 = [];
+array2 = array1;
+console.log(array2);
---
+++ practice13/tutorial.ts
@@ -0,0 +1,9 @@
+//const a:Number=3;
+//const b:Number=a;//tsでは変数の後ろに型を入れる
+//console.log(b);
+
+//let array1:Array<number>=[1,2,3,4];
+let array1:number[]=[1,2,3,4];
+let array2:number[]=[];
+array2=array1;
+console.log(array2);
